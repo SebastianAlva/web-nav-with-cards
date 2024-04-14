71d17c114d041e4c333391a19f7a5ed2a5fbf705
Sebastian Alva <98499668+SebastianAlva@users.noreply.github.com>
2024-04-14 16:15:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,53 @@
-
 let counter = 0;
-async function showComments(){
-    await(fetch('https://dummyjson.com/products'))
-    .then(res => res.json())
-    .then(json => {
-        /* createCards(json) */
-        try{
-            const cards_container = document.querySelector(".cards_container");
-            const products = json.products;
-            console.log(products[0].description)
-            const card = document.createElement("card");
-            const descInfo= document.createElement ("p");
-            
-            descInfo.textContent = `${products[counter].description}`;
-            card.appendChild(descInfo);
-            cards_container.appendChild(card)
-        }catch(e){
-            console.log("ERROR :ella es fanatica de lo sensual")
-        }
-    })
-}         
+const cards_container = document.querySelector(".cards_container");
+const card = document.createElement("card");
+const descInfo = document.createElement("p");
+descInfo.className = "description";
+const backward = document.createElement("button");
+const forward = document.createElement("button");
 /**
- * @description: is used to create each target in the webpage
- * @param {*} obj 
+ * @description:  creates the first slide of the target
+ * @returns json file
  */
-/* function createCards(obj){
-    const cards_container = document.querySelector(".cards_container");
-    const products = obj.products;
-    for(product of products){
-        const card = document.createElement("card");
-        const descInfo= document.createElement ("p");
-        descInfo.textContent = `${product.description}`
-        card.appendChild(descInfo);
-        cards_container.appendChild(card)
-    }
-}
+async function showComments() {
+    await (fetch('https://dummyjson.com/products'))
+        .then(res => res.json())
+        .then(json => {
+            try {
+                //create the first card
+                const products = json.products;
+                descInfo.textContent = `${products[counter].description}`;
+                backward.textContent = "<";
+                forward.textContent = ">";
+                card.appendChild(descInfo);
+                cards_container.appendChild(card)
+                cards_container.appendChild(backward);
+                cards_container.appendChild(forward);
+                forward.addEventListener("click", () => {
+                    if (counter != products.length - 1) {
+                        counter++;
+                        descInfo.textContent = `${products[counter].description}`
 
-function displayOneCard(obj){
-    
-    
-} */
-showComments();
+                    } else {
+                        counter = 0;
+
+                    }
+                }
+                );
+                backward.addEventListener("click", () => {
+                    if (counter != 0) {
+                        counter--;
+                        descInfo.textContent = `${products[counter].description}`
+
+                    } else {
+                        counter = products.length - 1;
+                        descInfo.textContent = `${products[counter].description}`
+                    }
+                });
+                return json;
+            } catch (e) {
+                console.log("ERROR :ella es fanatica de lo sensual")
+            }
+        })
+}
+const jsonFile = showComments();
